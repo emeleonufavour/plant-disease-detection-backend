1896f3c30c30e7e12686cace8aee1be626b7ed7a
Emele-Onu Favour <emeleonufavour15@gmail.com>
2024-03-30 09:07:27 +0100

    chore: initial commit

---
+++ src/main/java/com/pdd/PlantDiseaseDetection/PlantDiseaseDetectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pdd.PlantDiseaseDetection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantDiseaseDetectionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantDiseaseDetectionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdd/PlantDiseaseDetection/PlantDiseaseDetectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdd.PlantDiseaseDetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDiseaseDetectionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
